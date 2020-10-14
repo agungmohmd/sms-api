929d6cd026f707a15261df1fd726fab4220e4f3f
Mohammad Agung Perdana <mohmd.agung@gmail.com>
2020-10-14 23:06:54 +0700

    send sms after saving, save stringified sms dlr

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const mysql = require('mysql')
+const cors = require('cors')
+const axios = require('axios')
 
 app.use(bodyParser.json())
+app.use(cors())
 app.use(
     bodyParser.urlencoded({
         extended: true
     })
 )
-app.use(function (req, res, next){
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+app.use(function (req, res, next) {
     res.locals.connection = mysql.createConnection({
-        host     : 'localhost',
-		user     : 'root',
-		password : '',
-		database : 'smsdb'
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'smsdb'
     });
     res.locals.connection.connect();
     next();
@@ -33,55 +37,72 @@ app.get('/', (req, res) => {
 //     console.log(req, res)
 // });
 
-app.get('/sms', async(req, res) => {
+app.post('/test', (req, res) => {
+    let ep = `http://www.myvaluefirst.com/smpp/sendsms?to=${req.body.to}&text=${req.body.message}&username=demoindo1&from=${req.body.from}&password=http1313`
+    axios.get(ep)
+        .then(resx => {
+            console.log(resx.data)
+            res.send(resx.data)
+        })
+        .catch(err => res.send(err))
+})
+
+app.get('/sms', async (req, res) => {
     var data = {
-    "message": "",
-    "status": false,
-    "data": [],
-    "response_code": 200
+        "message": "",
+        "status": false,
+        "data": [],
+        "response_code": 200
     }
     let query = `select * from sendedsms`
     if (req.query.id) {
         query += ` where id = ${req.query.id}`
     }
     res.locals.connection.query(query, function (error, results, fields) {
-        if (error){
+        if (error) {
             res.send(data)
         };
         data.message = "OK";
         data.status = true;
         data.data = results;
-		res.send(data);
-	});
+        res.send(data);
+    });
 });
 
-app.post('/sms', function(req, res) {
+app.post('/sms', function (req, res) {
     var data = {
         "message": "",
         "status": false,
         "data": [],
         "response_code": 200
-        }
-        
-        if (req.body) {
-            let query =  `INSERT INTO sendedsms (send_from, send_to, mesage_sent, sended_on) 
+    }
+
+    if (req.body) {
+        let query = `INSERT INTO sendedsms (send_from, send_to, mesage_sent, sended_on) 
             VALUES ('${req.body.from}', '${req.body.to}', '${req.body.message}', '${req.body.sended_on}')`
 
-            console.log(query)
-            
-            res.locals.connection.query(query, function(error, results, fields) {
-                if (error) {
-                    res.send(data)
-                }
-                data.message = "OK";
-                data.status = true;
-                data.data = results;
-                res.send(data);
-            })
-        } else {
-            data.message = "Mohon Lengkapi Data"
-            res.send(data)
-        }
+        console.log(query)
+
+        res.locals.connection.query(query, function (error, results, fields) {
+            if (error) {
+                res.send(data)
+            }
+            let ep = `http://www.myvaluefirst.com/smpp/sendsms?to=${req.body.to}&text=${req.body.message}&username=demoindo1&from=${req.body.from}&password=http1313`
+            axios.get(ep)
+                .then(resx => {
+                    data.message = resx.data;
+                    data.status = true;
+                    data.data = results;
+                    res.send(data);
+                    // console.log(resx.data)
+                    // res.send(resx.data)
+                })
+                .catch(err => res.send(err))
+        })
+    } else {
+        data.message = "Mohon Lengkapi Data"
+        res.send(data)
+    }
 });
 
 app.get('/receipt', async (req, res) => {
@@ -90,19 +111,19 @@ app.get('/receipt', async (req, res) => {
         "status": false,
         "data": [],
         "response_code": 200
-        }
+    }
     console.log(req.query)
     let saveddlr = JSON.stringify(req.query)
-    let query =  `INSERT INTO smsdlr (dlr) 
+    let query = `INSERT INTO smsdlr (dlr) 
             VALUES ('${saveddlr}')`
-            res.locals.connection.query(query, function(error, results, fields){
-                if(error){
-                    res.send(data)
-                }
-                data.message = "OK";
-                data.status = true;
-                res.send(data);
-            })
+    res.locals.connection.query(query, function (error, results, fields) {
+        if (error) {
+            res.send(data)
+        }
+        data.message = "OK";
+        data.status = true;
+        res.send(data);
+    })
 });
 
 // function handleParams(params, res) {
